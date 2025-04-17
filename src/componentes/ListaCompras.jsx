b295079d8414371a50e6ed8de4741a2b670e3bc1
Serena Ludueña <sereluduena@gmail.com>
2025-04-17 17:20:09 -0300

    error en el contador

--- src/componentes/ListaCompras.jsx
+++ src/componentes/ListaCompras.jsx
@@ -29,7 +29,7 @@ export function ListaCompras({handleChange,añadirCompra, compra}){
                     </div>
                 </div>
                 
-                <button type="submit" onClick={()=>setCount()}>Agregar</button>
+                <button type="submit" onClick={()=>setCount(count)}>Agregar</button>
             </form>
             
         </div>
